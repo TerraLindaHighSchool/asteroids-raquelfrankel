dd053d39c336482dc9ee231988a48b3e89be0494
raquelfrankel <60672345+raquelfrankel@users.noreply.github.com>
2020-03-02 12:47:17 -0800

    asteroids lab 4
    complete

--- Space.java
+++ Space.java
@@ -88,6 +88,8 @@ public class Space extends World
     {
         int x = getWidth() / 2;
         int y = getHeight() / 2;
+         int currentScore = scoreCounter.getValue();
+        addObject(new ScoreBoard(currentScore), x ,y); 
     }
 
     /**
